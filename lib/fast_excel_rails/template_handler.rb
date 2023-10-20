86a63b51fc4da38836cad64112e67ebc512782a7
Steffan Sluis <steffan@feedbackfruits.com>
2023-10-20 14:13:07 +0200

    Allow passing fast_excel arguments to FastExcel.open

--- lib/fast_excel_rails/template_handler.rb
+++ lib/fast_excel_rails/template_handler.rb
@@ -8,11 +8,16 @@ module FastExcelRails
 
     def call(template, source = nil)
       <<-eos
+        # Expose FastExcel.open arguments as locals
+        fast_excel_filename = defined?(fast_excel_filename).nil? ? nil : fast_excel_filename
+        fast_excel_constant_memory = defined?(fast_excel_constant_memory).nil? ? false : fast_excel_constant_memory
+        fast_excel_default_format = defined?(fast_excel_default_format).nil? ? nil : fast_excel_default_format
+
         # Only build one shared workbook object so partials can take advantage
         # of the same workbook by default.
         root_view = false
         unless @fast_excel_rails_template_workbook
-          @fast_excel_rails_template_workbook = FastExcel.open
+          @fast_excel_rails_template_workbook = FastExcel.open(fast_excel_filename, constant_memory: fast_excel_constant_memory, default_format: fast_excel_default_format)
           root_view = true
         end
 
